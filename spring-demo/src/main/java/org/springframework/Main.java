471f41e649764ec418d4742429739e8c52e3d162
hudongdong <hudongdong668@hellobike.com>
2023-12-25 19:56:07 +0800

    新增spring测试

---
+++ spring-demo/src/main/java/org/springframework/Main.java
@@ -0,0 +1,12 @@
+package org.springframework;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationcontext.xml");
+		Person person = applicationContext.getBean(Person.class);
+
+		System.out.println(person.getName());
+	}
+}
